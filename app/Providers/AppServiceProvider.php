71b48bb064354e0dc99c7ec613c852848e0bba82
Nixie <nyxscream@gmail.com>
2018-06-19 10:49:44 +0200

    database canviada

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -14,7 +14,7 @@ class AppServiceProvider extends ServiceProvider{
      */
     public function boot(){
         URL::forceScheme('https');
-        //Schema::defaultStringLength(191);
+        Schema::defaultStringLength(191);
     }
 
     /**
