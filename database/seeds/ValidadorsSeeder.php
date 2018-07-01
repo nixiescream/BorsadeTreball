306f473ca8f586146143646e98efeeecf9411556
Nixie <nyxscream@gmail.com>
2018-07-01 20:33:28 +0200

    validador arreglat

--- database/seeds/ValidadorsSeeder.php
+++ database/seeds/ValidadorsSeeder.php
@@ -10,7 +10,7 @@ class ValidadorsSeeder extends Seeder{
      */
     public function run(){
 
-        $user1 = App\User::create([
+        /*$user1 = App\User::create([
             'name' => 'validador1',
             'email' => 'validador1@example.com',
             'password' =>  Hash::make('validador1'),
@@ -21,6 +21,20 @@ class ValidadorsSeeder extends Seeder{
             'user_id' => $user1['id'],
             'email' => 'validador1@example.com',
             'password' =>  Hash::make('validador1'),
+            'rol' => 'validador'
+        ]);*/
+
+        $user2 = App\User::create([
+            'name' => 'validador2',
+            'email' => 'validador2@example.com',
+            'password' =>  Hash::make('validador2'),
+            'rol' => 'validador',
+            'validat' => 1
+        ]);
+        App\Validador::create([
+            'user_id' => $user2['id'],
+            'email' => 'validador2@example.com',
+            'password' =>  Hash::make('validador2'),
             'rol' => 'validador'
         ]);
     }
